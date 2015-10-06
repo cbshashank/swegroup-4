91251f97cb16859236a2d44a537a804423c11177
cbarrieau232 <cbarrieau@gmail.com>
2015-10-06 05:56:07 -0400

    Revert "Revert "Initial Checkin""
    This reverts commit e2337b51b3dee955016cbc207bae4f9a955a1fa7.

---
+++ OWLwebsite/App_Code/FloraObj.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for FloraObj
+/// </summary>
+public class FloraObj
+{
+    private int leafCount;
+    private string color;
+    private string name;
+    private string extra;
+
+    public int LeafCount
+    {
+        get
+        {
+            return leafCount;
+        }
+        set
+        {
+            leafCount = value;
+        }
+    }
+
+    public string Color
+    {
+        get
+        {
+            return color;
+        }
+        set
+        {
+            color = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+
+    public string Extra
+    {
+        get { return extra; }
+        set { extra = value; }
+    }
+
+    public FloraObj()
+    {
+        
+    }
+}
---
+++ TestOWL/src/TestOWL/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TestOWL
+{
+    public class Program
+    {
+        void GenerateGetRequest()
+        {
+            string url = "http://localhost:30992";
+            HttpWebRequest GETRequest = (HttpWebRequest)WebRequest.Create(url);
+            GETRequest.Method = "POST";
+            
+            Console.WriteLine("Sending POST Request");
+            using (var streamWriter = new StreamWriter(GETRequest.GetRequestStream()))
+            {
+                string json = "{\"LeafCount\":\"4\"," +
+                              "\"Color\":\"Green\","  +
+                              "\"Name\":\"Ted\"," +
+                              "\"Extra\":\"Notes\"" +
+                              "}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+
+            HttpWebResponse GETResponse = (HttpWebResponse)GETRequest.GetResponse();
+            
+            Stream GETResponseStream = GETResponse.GetResponseStream();
+            StreamReader sr = new StreamReader(GETResponseStream);
+
+            Console.WriteLine("Response from Server");
+            Console.WriteLine(sr.ReadToEnd());
+            Console.ReadLine();
+        }
+
+        public void Main(string[] args)
+        {
+            GenerateGetRequest();
+        }
+    }
+}
