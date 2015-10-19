c1909769586b21b92dc0b08498699fb6c8d5c781
Scott <phoenixhwk@comcast.net>
2015-10-18 20:51:02 -0400

    * Modified test message * modified result parsing and display

--- owl-front/src/controllers/QuestionController.js
+++ owl-front/src/controllers/QuestionController.js
@@ -1,26 +1,34 @@
 define(function(){
-    var us_state = {text: 'Where is the plant in the US?', 
+    var us_state = {term: 'USState',
+                text: 'Where is the plant in the US?',
                 options: ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "DC", "FM", "GU", "MH", "MP", "PW", "PR", "VI"
                 ]};
-    var type = {text: 'What type of plant is it?', 
+    var type = {term: 'Type',
+                text: 'What type of plant is it?',
                 options: ["Forb/herb", "Graminoid", "Lichenous", "Nonvascular", "Shrub", "Subshrub", "Tree", "Vine"
                 ]};
-    var color_flower = {text: 'What color are the flowers?', 
+    var color_flower = {term: 'ColorFlower',
+                text: 'What color are the flowers?',
                 options: ["Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
                 ]};
-    var color_foliage = {text: 'What color are the leaves?', 
+    var color_foliage = {term: 'ColorFoliage',
+                text: 'What color are the leaves?',
                 options: ["Dark Green", "Green", "Grey-Green", "Red", "White-Gray", "Yellow-Green"
                 ]};
-    var color_fruit_seed = {text: 'What color are the fruit or seeds?', 
+    var color_fruit_seed = {term: 'ColorFruitSeed',
+                text: 'What color are the fruit or seeds?',
                 options: ["Black", "Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
                 ]};
-    var shape = {text: 'What shape does the plant have?', 
+    var shape = {term: 'Shape',
+                text: 'What shape does the plant have?',
                 options: ["Climbing", "Columnar", "Conical", "Decumbent", "Erect", "Irregular", "Oval", "Prostrate", "Rounded", "Semi-Erect", "Vase"
                 ]};
-    var texture_foliage = {text: 'What kind of texture do the leaves have?', 
+    var texture_foliage = {term: 'TextureFoliage',
+                text: 'What kind of texture do the leaves have?',
                 options: ["Fine", "Medium", "Coarse"
                 ]};
-    var pattern = {text: 'What kind of pattern does the plant have?', 
+    var pattern = {term: 'Pattern',
+                text: 'What kind of pattern does the plant have?',
                 options: ["Dicot", "Fern", "Green Alga", "Gymnosperm", "Hornwort", "Horsetail", "Lichen", "Liverwort", "Lycopod", "Monocot", "Moss", "Quillwort", "Red Algae", "Whisk-fern"
                 ]};
     var questions = [us_state, type, 
@@ -29,7 +37,7 @@ define(function(){
 
     function QuestionController(view, clientCommunicationModule){
         //var testInput = {"Color":"Red","Shape":"Vine","Texture":"Rough"};
-        var testInput2 = {"us_state":"MA", "type":"vine", "shape":"Rounded"};
+        var testInput2 = {"ColorFlower":"Yellow", "TextureFoliage":"Fine", "Shape":"Rounded"};
 
         view.setModel({
             questions
@@ -38,7 +46,7 @@ define(function(){
         });
 
         view.onAnswer(function(){
-            clientCommunicationModule.sendCharacteristics(testInput);
+            clientCommunicationModule.sendCharacteristics(testInput2);
            //alert('send to');
         });
     }
