6fc2b7230c029e25b7a945d6e14c195e976cd26f
Arthur Pitzer <pitzer@bu.edu>
2015-11-02 15:53:35 -0500

    Fetch questions is now responsability of the CommunicationModule, different widgets for different questions

--- owl-front/src/ClientCommunicationModule.js
+++ owl-front/src/ClientCommunicationModule.js
@@ -2,15 +2,16 @@
  * Created by Scott on 10/7/2015.
  */
 
-define(['jquery','ResultDisplay'], function($,ResultDisplay){
+define(['jquery', 'ResultDisplay'], function ($, ResultDisplay) {
 
-    function ClientCommunicationModule(){}
+    function ClientCommunicationModule() {
+    }
 
     var output_display = new ResultDisplay();
     var serverAddress = "http://localhost:32297"
 //    var serverAddress = "http://localhost:3000"
 
-    ClientCommunicationModule.prototype.sendCharacteristics = function(output){
+    ClientCommunicationModule.prototype.sendCharacteristics = function (output) {
         var questionText = document.getElementById('form');
 
         $.ajax({
@@ -24,9 +25,63 @@ define(['jquery','ResultDisplay'], function($,ResultDisplay){
 //                alert("result=" + result);
                 output_display.updateResultDisplay(result);
             },
-            error: (function() { alert("Error"); })	// When Service call fails
+            error: (function () {
+                alert("Error");
+            })	// When Service call fails
         });
 
+    };
+
+    ClientCommunicationModule.prototype.getQuestions = function () {
+        return [
+            {
+                term: 'USState',
+                text: 'Where is the plant in the US?',
+                options: ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "DC", "FM", "GU", "MH", "MP", "PW", "PR", "VI"
+                ]
+            },
+            {
+                term: 'Type',
+                text: 'What type of plant is it?',
+                options: ["Forb/herb", "Graminoid", "Lichenous", "Nonvascular", "Shrub", "Subshrub", "Tree", "Vine"
+                ]
+            },
+            {
+                term: 'ColorFlower',
+                text: 'What color are the flowers?',
+                options: ["Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
+                ]
+            },
+            {
+                term: 'ColorFoliage',
+                text: 'What color are the leaves?',
+                options: ["Dark Green", "Green", "Grey-Green", "Red", "White-Gray", "Yellow-Green"
+                ]
+            },
+            {
+                term: 'ColorFruitSeed',
+                text: 'What color are the fruit or seeds?',
+                options: ["Black", "Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
+                ]
+            },
+            {
+                term: 'Shape',
+                text: 'What shape does the plant have?',
+                options: ["Climbing", "Columnar", "Conical", "Decumbent", "Erect", "Irregular", "Oval", "Prostrate", "Rounded", "Semi-Erect", "Vase"
+                ]
+            },
+            {
+                term: 'TextureFoliage',
+                text: 'What kind of texture do the leaves have?',
+                options: ["Fine", "Medium", "Coarse"
+                ]
+            },
+            {
+                term: 'Pattern',
+                text: 'What kind of pattern does the plant have?',
+                options: ["Dicot", "Fern", "Green Alga", "Gymnosperm", "Hornwort", "Horsetail", "Lichen", "Liverwort", "Lycopod", "Monocot", "Moss", "Quillwort", "Red Algae", "Whisk-fern"
+                ]
+            }];
     };
 
     return ClientCommunicationModule;
