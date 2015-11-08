c43a0326e530e49c57cd483bc694453f1ed94c74
Wyliena <wguan12@bu.edu>
2015-11-07 20:19:20 -0500

    createDB before populateDB
    Was auditing code.  Before populating the OWL DB, it has to be created.

---
+++ createDB/createDB/createDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace populateDB
+{
+    public class createDB
+    {
+        public static void Main(string[] args)
+        {
+            System.Data.SqlClient.SqlConnection tmpConn;
+            string sqlCreateDBQuery;
+            // SqlConnection tmpConn = new SqlConnection();
+            // tmpConn = new SqlConnection("server=SQLExpress;");
+
+            tmpConn = new SqlConnection("user id=username;" + "password=password;" + "server=.\\SQLExpress;" + "Trusted_Connection=yes;");
+
+            // tmpConn.ConnectionString = "SERVER = SQLExpress;";
+            sqlCreateDBQuery = "CREATE DATABASE OWL";
+
+            SqlCommand myCommand = new SqlCommand(sqlCreateDBQuery, tmpConn);
+            try
+            {
+                tmpConn.Open();
+                Console.WriteLine(sqlCreateDBQuery);
+                myCommand.ExecuteNonQuery();
+                Console.WriteLine("Database has been created successfully!");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                tmpConn.Close();
+            }
+            return;
+        }
+    }
+}
