26f8b65cb4696a0bc1d35cd5ff4eb3a8bfca2d29
cbarrieau232 <cbarrieau@gmail.com>
2015-11-14 16:10:27 -0500

    Fix to url code for image links
    Added in code for url image links.  In addition, updated client
    communication test to test the image urls (uses test plant object)

--- owl-front/tests/ClientCommunicationModule - test.js
+++ owl-front/tests/ClientCommunicationModule - test.js
@@ -21,7 +21,7 @@ define(['jquery', 'ResultDisplay', 'views/AdminResultDisplay'], function ($, Res
             dataType: "text",
             success: function (in_data) {//On Successful service call
                 //alert("input = " + in_data);
-                var result = JSON.parse(in_data); 
+                var result = JSON.parse(in_data);
                 //                alert("result=" + result);
                 output_display.updateResultDisplay(result);
             },
@@ -33,8 +33,22 @@ define(['jquery', 'ResultDisplay', 'views/AdminResultDisplay'], function ($, Res
     };
 
     ClientCommunicationModule.prototype.getQuestions = function (onReceive) {
-        alert("GetQuestion");
+       // $.ajax({
+        //    url: serverAddress, 		// Location of the service
+         //   type: "GET", 		//GET or POST or PUT or DELETE verb
+          //  dataType: "text",
+           // success: function (in_data) {//On Successful service call
+            //    alert(in_data);
+             //   var result = JSON.parse(in_data);
+              //  onReceive(result);
+            //},
+                //error: (function () {
                     onReceive([
+                        {
+                            term: 'PlantId',
+                            text: 'What is the plant ID?',
+                            options: []
+                        },
                         {
                             term: 'Name',
                             text: 'What is the plant name?',
@@ -48,53 +62,55 @@ define(['jquery', 'ResultDisplay', 'views/AdminResultDisplay'], function ($, Res
                         {
                             term: 'USState',
                             text: 'Where is the plant in the US?',
-                            options: ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "DC", "FM", "GU", "MH", "MP", "PW", "PR", "VI"
-                            ]
+                            options: ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "DC", "FM", "GU", "MH", "MP", "PW", "PR", "VI"]
+                                
                         },
                         {
                             term: 'Type',
                             text: 'What type of plant is it?',
-                            options: ["Forb/herb", "Graminoid", "Lichenous", "Nonvascular", "Shrub", "Subshrub", "Tree", "Vine"
-                            ]
+                            options: ["Forb/herb", "Graminoid", "Lichenous", "Nonvascular", "Shrub", "Subshrub", "Tree", "Vine"],
+                            urls:["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         },
                         {
                             term: 'ColorFlower',
                             text: 'What color are the flowers?',
-                            options: ["Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
-                            ]
+                            options: ["Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"],
+                            urls: ["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         },
                         {
                             term: 'ColorFoliage',
                             text: 'What color are the leaves?',
-                            options: ["Dark Green", "Green", "Grey-Green", "Red", "White-Gray", "Yellow-Green"
-                            ]
+                            options: ["Dark Green", "Green", "Grey-Green", "Red", "White-Gray", "Yellow-Green"],
+                            urls:["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         },
                         {
                             term: 'ColorFruitSeed',
                             text: 'What color are the fruit or seeds?',
-                            options: ["Black", "Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"
-                            ]
+                            options: ["Black", "Blue", "Brown", "Green", "Orange", "Purple", "Red", "White", "Yellow"],
+                            urls: ["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         },
                         {
                             term: 'Shape',
                             text: 'What shape does the plant have?',
-                            options: ["Climbing", "Columnar", "Conical", "Decumbent", "Erect", "Irregular", "Oval", "Prostrate", "Rounded", "Semi-Erect", "Vase"
-                            ]
+                            options: ["Climbing", "Columnar", "Conical", "Decumbent", "Erect", "Irregular", "Oval", "Prostrate", "Rounded", "Semi-Erect", "Vase"],
+                            urls: ["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg","http://companionplants.com/images/small-plant2.jpg"]
+                            
                         },
                         {
                             term: 'TextureFoliage',
                             text: 'What kind of texture do the leaves have?',
-                            options: ["Fine", "Medium", "Coarse"
-                            ]
+                            options: ["Fine", "Medium", "Coarse"],
+                            urls: ["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         },
                         {
                             term: 'Pattern',
                             text: 'What kind of pattern does the plant have?',
-                            options: ["Dicot", "Fern", "Green Alga", "Gymnosperm", "Hornwort", "Horsetail", "Lichen", "Liverwort", "Lycopod", "Monocot", "Moss", "Quillwort", "Red Algae", "Whisk-fern"
-                            ]
+                            options: ["Dicot", "Fern", "Green Alga", "Gymnosperm", "Hornwort", "Horsetail", "Lichen", "Liverwort", "Lycopod", "Monocot", "Moss", "Quillwort", "Red Algae", "Whisk-fern"],
+                            urls: ["http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg", "http://companionplants.com/images/small-plant2.jpg"]
                         }]);
-                
-    }
+                //})
+        }//)
+   // })
 
     ClientCommunicationModule.prototype.insertData = function (data) {
         var out = JSON.stringify(data);
