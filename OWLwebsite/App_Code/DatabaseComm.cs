9087000979359a8dfdb647e0af2a86dc2066c232
abnv418 <abhinav0418@gmail.com>
2015-11-17 23:57:55 -0500

    Insert code and delete code
    Insert code and delete code

--- OWLwebsite/App_Code/DatabaseComm.cs
+++ OWLwebsite/App_Code/DatabaseComm.cs
@@ -372,15 +372,23 @@ public class DatabaseComm
 
     public string getplantid(FloraObj FLO) // infinite loop for checking
     {
-        String plantid = null;
-        plantid = FLO.Name.Substring(0,4);
-        for(int i=1; ; i++)
+        int i=1;
+        string plantid = "";
+        if (FLO.Name.Length > 4)
+        {
+
+            plantid = FLO.Name.Substring(0, 4);
+        }
+        else
+            plantid = FLO.Name;
+            plantid = plantid + i;
+        for (i=1;i<10; i++) //added the condition i <10 for preventing the crash
         {
             plantid = plantid + i;
             if (checkplantid(plantid) == false)
                 return plantid;
         }
-
+        return plantid;
 
     }
 
@@ -412,6 +420,7 @@ public class DatabaseComm
                   (ReturnResult["plantid"].ToString() == plantid)
                 {
                     found = true;
+                    
                     return found;
 
                 }
@@ -427,14 +436,14 @@ public class DatabaseComm
 
     public void Insert(FloraObj FLO)
     {
-
+        string plantid;
         FLO.UserName = "Admin";
         FLO.Password = "Admin";
 
         if (checkAdminLogin(FLO) == "Authenticated")
         {
 
-
+            
             // check user name & password here ok or not
             //using parametirized query
             string sqlInserString =
@@ -445,10 +454,55 @@ public class DatabaseComm
             command.Connection = conn;
             command.Connection.Open();
             command.CommandText = sqlInserString;
+            if (FLO.Type == null)
+                FLO.Type = "";
+            if (FLO.Location == null)
+                FLO.Location = "";
+
             if (FLO.ColorFoliage == null)
                 FLO.ColorFoliage = "";
+            if (FLO.ColorFlower == null)
+                FLO.ColorFlower = "";
+            if (FLO.PlantId == null)
+                FLO.PlantId = "";
+            if (FLO.ColorFruitSeed == null)
+                FLO.ColorFruitSeed  = "";
+            if (FLO.TextureFoliage == null)
+                FLO.TextureFoliage = "";
+            if (FLO.Shape == null)
+                FLO.Shape= "";
+            if (FLO.Pattern == null)
+                FLO.Pattern = "";
+            if (FLO.ImageURL == null)
+                FLO.ImageURL = "";
+            if (FLO.Name == null)
+                FLO.Name = "";
 
-            SqlParameter plant_id = new SqlParameter("@plant_id", getplantid(FLO)); 
+            if (FLO.Name.Length > 20)
+                FLO.Name = FLO.Name.Substring(0, 20);
+            if (FLO.Name.Length > 20)
+                FLO.PlantId = FLO.PlantId.Substring(0, 20);
+            if (FLO.Pattern.Length > 20)
+                FLO.Pattern = FLO.Pattern.Substring(0, 20);
+            if (FLO.Shape.Length > 20)
+                FLO.Shape = FLO.Shape.Substring(0, 20);
+            if (FLO.ImageURL.Length > 20)
+                FLO.ImageURL = FLO.ImageURL.Substring(0, 20);
+            if (FLO.ColorFlower.Length > 20)
+                FLO.ColorFlower=FLO.ColorFlower.Substring(0, 20);
+            if (FLO.ColorFoliage.Length > 20)
+                FLO.ColorFoliage = FLO.ColorFoliage.Substring(0, 20);
+            if (FLO.ColorFruitSeed.Length > 20)
+                FLO.ColorFruitSeed = FLO.ColorFruitSeed.Substring(0, 20);
+            if (FLO.TextureFoliage.Length > 20)
+                FLO.TextureFoliage = FLO.TextureFoliage.Substring(0, 20);
+            if (FLO.Type.Length > 20)
+                FLO.Type = FLO.Type.Substring(0, 20);
+            if (FLO.Location.Length > 20)
+                FLO.Location = FLO.Location.Substring(0, 20);
+
+            plantid = getplantid(FLO);
+            SqlParameter plant_id = new SqlParameter("@plant_id",plantid ); 
             SqlParameter Name = new SqlParameter("@Name", FLO.Name);
             SqlParameter Color_flower = new SqlParameter("@Color_flower", FLO.ColorFlower);
             SqlParameter Color_foliage = new SqlParameter("@Color_foliage", FLO.ColorFoliage);
@@ -466,19 +520,103 @@ public class DatabaseComm
                 plant_id,Name, Color_flower,Color_foliage,Color_fruit_seed,texture, shape, pattern,image});
             command.ExecuteNonQuery();
             command.Connection.Close();
+            insertlocation(plantid, FLO.Location);
+            insertplanttype(plantid, FLO.Type);
 
         }
         // Check Admin USer & pass. 
     }
 
-    public void Delete(FloraObj FLO)
+    public void insertlocation(string plantid, string location)
+
     {
-        //using parametirized query
-        string sqlInserString =
-          "DELETE FROM plant WHERE plant_id=@plant_id ";
+       
+        string sqlInserString ="INSERT INTO location (plant_id,location) VALUES (@plant_id,@location)";
+
+            SqlConnection conn = new SqlConnection(conn_string);
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.Connection.Open();
+            command.CommandText = sqlInserString;
+
+            SqlParameter plant_id = new SqlParameter("@plant_id",plantid);
+            SqlParameter Location = new SqlParameter("@location", location);
+          
+            command.Parameters.AddRange(new SqlParameter[]{plant_id,Location});
+            command.ExecuteNonQuery();
+            command.Connection.Close();
+
+        }
+
+    public void insertplanttype(string plantid, string type)
+
+    {
+
+        string sqlInserString = "INSERT INTO location (plant_id,type) VALUES (@plant_id,@type)";
 
         SqlConnection conn = new SqlConnection(conn_string);
+        SqlCommand command = new SqlCommand();
+        command.Connection = conn;
+        command.Connection.Open();
+        command.CommandText = sqlInserString;
 
+        SqlParameter plant_id = new SqlParameter("@plant_id", plantid);
+        SqlParameter Type = new SqlParameter("@location", type);
+
+        command.Parameters.AddRange(new SqlParameter[] { plant_id, Type });
+        command.ExecuteNonQuery();
+        command.Connection.Close();
+
+    }
+
+    public void Delete(FloraObj FLO) // delete for plant table.
+    {
+        //using parametirized query
+        string sqlInserString = "DELETE  FROM plant WHERE plant_id=@plant_id ";
+
+        SqlConnection conn = new SqlConnection(conn_string);
+        SqlCommand command = new SqlCommand();
+        command.Connection = conn;
+        command.Connection.Open();
+        command.CommandText = sqlInserString;
+
+        SqlParameter plant_id = new SqlParameter("@plant_id", FLO.PlantId);
+
+
+        command.Parameters.AddRange(new SqlParameter[]{
+                plant_id});
+        command.ExecuteNonQuery();
+        command.Connection.Close();
+
+    }
+
+    public void Deletetype(FloraObj FLO) // delete for plant table.
+    {
+        //using parametirized query
+        string sqlInserString = "DELETE  FROM type WHERE plant_id=@plant_id ";
+
+        SqlConnection conn = new SqlConnection(conn_string);
+        SqlCommand command = new SqlCommand();
+        command.Connection = conn;
+        command.Connection.Open();
+        command.CommandText = sqlInserString;
+
+        SqlParameter plant_id = new SqlParameter("@plant_id", FLO.PlantId);
+
+
+        command.Parameters.AddRange(new SqlParameter[]{
+                plant_id});
+        command.ExecuteNonQuery();
+        command.Connection.Close();
+
+    }
+
+    public void Deletelocation(FloraObj FLO) // delete for plant table.
+    {
+        //using parametirized query
+        string sqlInserString = "DELETE  FROM location WHERE plant_id=@plant_id ";
+
+        SqlConnection conn = new SqlConnection(conn_string);
         SqlCommand command = new SqlCommand();
         command.Connection = conn;
         command.Connection.Open();
