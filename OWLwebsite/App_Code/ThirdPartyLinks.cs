544a3a4768802a77d75bfd47d97f209c68baad8f
cbarrieau232 <cbarrieau@gmail.com>
2015-11-23 00:07:58 -0500

    Switch from Google to DuckDuckGo for results
    Switched to DuckDuckGo for results because DuckDuckGo can be displayed
    in an iframe.

--- OWLwebsite/App_Code/ThirdPartyLinks.cs
+++ OWLwebsite/App_Code/ThirdPartyLinks.cs
@@ -13,16 +13,21 @@ public class ThirdPartyLinks
        
     public static string GoogleLink(string query)
     {
-        string googlelink = "https://www.google.com/search?q/";
+        //string googlelink = "https://www.google.com/search?q=";
+        //string googlelink = "https://en.wikipedia.org/wiki/";
+        string googlelink = "https://duckduckgo.com/?q=";
         googlelink += query;
+        //googlelink += "&output=embed";
         return googlelink;
     }
 
     public static string GoogleImageLink(string query)
     {
-        string googlelink = "https://www.google.com/search?q/";
+        //string googlelink = "https://www.google.com/search?q=";
+        string googlelink = "https://duckduckgo.com/?q=";
         googlelink += query;
-        googlelink += "&source=lnms&tbm=isch";
+        //googlelink += "&source=lnms&tbm=isch";
+        googlelink += "&iax=1&ia=images";
         return googlelink;
     }
 
