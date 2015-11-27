c637c5443659f11af77540bf9576e672e92e4e50
Arthur Pitzer <pitzer@bu.edu>
2015-11-26 19:49:44 -0500

    forgot to remove the mock

--- owl-front/src/main.js
+++ owl-front/src/main.js
@@ -1,5 +1,5 @@
 require([
-        'MockClientCommunicationModule',//'ClientCommunicationModule',//
+        'ClientCommunicationModule',//'MockClientCommunicationModule',//
         'controllers/QuestionController', 'views/QuestionView',
         'controllers/ResultController', 'views/ResultView'
     ],
