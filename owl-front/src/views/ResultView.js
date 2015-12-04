e81469a179fbc3baa585d1796b8eca700c4eea07
Arthur Pitzer <pitzer@bu.edu>
2015-12-04 16:02:25 -0500

    'More results' option on result display

--- owl-front/src/views/ResultView.js
+++ owl-front/src/views/ResultView.js
@@ -7,15 +7,14 @@ define(function () {
     ResultView.prototype.setModel = function (model) {
         this.model = model;
         this.clearUI();
-		this.className = "row";
+        this.className = "row";
         for (var i = 0; i < model.length; i++) {
             var item = model[i];
             var itemDisplay = document.createElement("div");
-			itemDisplay.className = "col-xs-4 col-md-3";
+            itemDisplay.className = "col-xs-4 col-md-3";
             itemDisplay.appendChild(this.displayName(item.Name));
             itemDisplay.appendChild(this.displayImage(item.ImageURL));
-            //itemDisplay.appendChild(this.displayGoogleResults(item.GoogleURL));
-            //itemDisplay.appendChild(this.displayGoogleImages(item.GoogleImageURL));
+            itemDisplay.appendChild(this.displayMoreResults(item.GoogleURL, item.GoogleImageURL));
             this.resultDisplay.appendChild(itemDisplay);
         }
     };
@@ -24,7 +23,7 @@ define(function () {
         this.resultDisplay.innerHTML = "";
     };
 
-    ResultView.prototype.displayName = function(name){
+    ResultView.prototype.displayName = function (name) {
         var header = document.createElement("h4");
         header.className = "text-left";
         header.innerHTML = name;
@@ -34,26 +33,24 @@ define(function () {
     ResultView.prototype.displayImage = function (imageURL) {
         var img = document.createElement("img");
         img.src = imageURL;
-		img.width = "160";
-		img.height = "160";
+        img.width = "160";
+        img.height = "160";
         var paragraph = document.createElement("p");
         paragraph.appendChild(img);
-		
+
         return paragraph;
     };
 
-    ResultView.prototype.displayGoogleResults = function (googleURL) {
-        var frame = document.createElement("iframe");
-        frame.className = "span5";
-        frame.src = googleURL;
-        return frame;
-    };
-
-    ResultView.prototype.displayGoogleImages = function (googleImageURL) {
-        var frame = document.createElement("iframe");
-        frame.className = "span5";
-        frame.src = googleImageURL;
-        return frame;
+    ResultView.prototype.displayMoreResults = function (googleURL, googleImageURL) {
+        var btn = document.createElement("button");
+        btn.type = "button";
+        btn.innerHTML = "More Results";
+        btn.onclick = function () {
+            window.open(googleURL);
+            window.open(googleImageURL);
+        };
+        btn.className = "span5";
+        return btn;
     };
 
     ResultView.prototype.getModel = function () {
